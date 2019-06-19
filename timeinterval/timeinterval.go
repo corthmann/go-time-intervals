15a636270689f8d4bbbf50e675e0d14379c6788e
Christian Orthmann <christian.orthmann@hotmail.com>
2019-06-19 10:28:41 +0200

    remove interfaces

--- timeinterval/timeinterval.go
+++ timeinterval/timeinterval.go
@@ -23,37 +23,6 @@ const TypeDuration formatType = 2
 const durationWeek = 7 * 24 * time.Hour
 const durationDay = 24 * time.Hour
 
-// TimeInterval is an interface that describes the API supported
-// for all intervals implemented in the timeinterval package.
-type TimeInterval interface {
-	// StartsAt returns the time the interval begins or nil if it does not have a lower bound.
-	StartsAt() *time.Time
-	// EndsAt returns the time the interval ends or nil if it does not have an upper bound.
-	EndsAt() *time.Time
-	// Duration returns the duration of the interval or nil if it is unbounded.
-	Duration() *time.Duration
-
-	// Started returns a boolean indicating if the interval have begun at the given time.
-	Started(t time.Time) bool
-	// Ended returns a boolean indicating if the interval have ended at the given time.
-	Ended(t time.Time) bool
-	// In returns a boolean indicating if the given time is while the interval is active (Started and not Ended)
-	In(t time.Time) bool
-
-	// ISO8601 returns the interval formatted according to ISO-8601 and an error if formatting fails.
-	ISO8601() (string, error)
-}
-
-// RepeatingTimeInterval is an interface that describes the API supported by intervals with repeating events.
-type RepeatingTimeInterval interface {
-	// TimeInterval means that this interface supports the TimeInterval API.
-	TimeInterval
-	// Next returns the time of the next occurrence/event relative to the given time.
-	// the StartsAt time is returned if the interval have not Started yet.
-	// nil is returned if the interval has Ended.
-	Next(t time.Time) *time.Time
-}
-
 // ParseIntervalISO8601 accepts a string with the ISO8601 "interval" format
 // and returns an Interval and an error if parsing of the string failed.
 // See: ref: https://en.wikipedia.org/wiki/ISO_8601#Time_intervals
